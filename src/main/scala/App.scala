2bf38f983f35ecc987d3f408e2658f62cfdca74f
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-07-01 12:54:42 +0200

    Create Task to extract config

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,14 +1,16 @@
 package acme
 
 import knobs._
+import scalaz.concurrent.Task
 
 case class MyConfig(driver: String, url: String, username: String, password: String)
 
 object App {
   def main(args: Array[String]) {
 
-    val config: Config = loadImmutable(List(classPathConfig)).run
+    val configTask: Task[Config] = loadImmutable(List(classPathConfig))
 
+    val config = configTask.run
     val driver: String = config.require[String]("database.driver")
     val url: String = config.require[String]("database.url")
     val username: String = config.require[String]("database.username")
