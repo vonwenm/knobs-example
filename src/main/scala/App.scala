a14b93ebfb8d88c3e62753131c38c06f5207f30c
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-07-01 17:00:59 +0200

    Minify example

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -3,62 +3,12 @@ package acme
 import knobs._
 import scalaz.concurrent.Task
 
-import java.io.File
-
-case class MyConfig(driver: String, url: String, username: String, password: String)
-
-object App {
-  def main(args: Array[String]) {
-
-    val configName = "config.hocon"
-    val configPath = new File(cwd, configName)
-
-    val defaultConfigTask = loadImmutable(List(classPathConfig(configName)))
-    val fileConfigTask = loadImmutable(List(filePathConfig(configPath)))
-
-    defaultConfigTask.map { defaultConfig =>
-      fileConfigTask.map { fileConfig =>
-        val config = defaultConfig ++ fileConfig
-
-        val driver: String = config.require[String]("database.driver")
-        val url: String = config.require[String]("database.url")
-        val username: String = config.require[String]("database.username")
-        val password: String = config.require[String]("database.password")
-
-        val myConfig = MyConfig(driver, url, username, password)
-        println(myConfig)
-        println("foo")
-      }.run
-    }.run
-
-    val xxx: Task[Unit] = for {
-      defaultConfig <- defaultConfigTask
-      fileConfig <- fileConfigTask
+object App extends App {
+    val foo = for {
+      config <- loadImmutable(List(ClassPathResource("config.hocon").required))
     } yield {
-        val config = defaultConfig ++ fileConfig
-
-        val driver: String = config.require[String]("database.driver")
-        val url: String = config.require[String]("database.url")
-        val username: String = config.require[String]("database.username")
-        val password: String = config.require[String]("database.password")
-
-        val myConfig = MyConfig(driver, url, username, password)
-        println(myConfig)
-        println("bar")
-      }
-    xxx.run
-  }
-
-  def filePathConfig(path: File) = {
-    FileResource(path).optional
-  }
-
-  def classPathConfig(configName: String) = {
-    val configPath = configName
-    ClassPathResource(configPath).required
-  }
-
-  def cwd = new File(".").getCanonicalFile()
-
+      println(config.require[String]("key"))
+    }
+    foo.run
 }
 
